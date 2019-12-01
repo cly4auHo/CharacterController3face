58f83ad34e6252cbcf95ab3c9ed7e97543e344cf
cly4auHo <arsen.dmitriev96@gmail.com>
2019-12-01 03:42:44 +0300

    hz

---
+++ Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 dPos;
+    private Vector3 pos;
+    private bool open;
+
+    public void Operate()
+    {
+        //pos = open ? transform.position - dPos : pos = transform.position + dPos;
+
+        if (open) 
+        {
+            pos = transform.position - dPos;
+        }
+        else
+        {
+            pos = transform.position + dPos;
+        }
+
+        transform.position = pos;
+        open = !open;
+    }
+}
